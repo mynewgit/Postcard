4eeda7736122d1f0bc6aff6717526dcc9bd9cb9e
Ryan McRae <rmcrae@Ryan-McRaes-MacBook-Pro.local>
2015-06-17 12:39:28 -0300

    challenge  test

--- Postcard/ViewController.swift
+++ Postcard/ViewController.swift
@@ -12,7 +12,7 @@ class ViewController: UIViewController {
     @IBOutlet weak var messageLabel: UILabel!
     @IBOutlet weak var enterNameTextField: UITextField!
     @IBOutlet weak var enterMessagetextfield: UITextField!
-    
+    @IBOutlet weak var nameLabel: UILabel!
     @IBOutlet weak var mailButton: UIButton!
     
     override func viewDidLoad() {
@@ -28,13 +28,26 @@ class ViewController: UIViewController {
 
     @IBAction func sendMailButtonPressed(sender: UIButton) {
         //testing commits
+        nameLabel.hidden = false
         messageLabel.hidden = false
+        nameLabel.text = "Hi, \(enterNameTextField.text) ,"
+        nameLabel.textColor = UIColor.blueColor()
+        enterNameTextField.text = ""
+        enterNameTextField.resignFirstResponder()
         messageLabel.text = enterMessagetextfield.text
         messageLabel.textColor = UIColor.redColor()
         enterMessagetextfield.text = ""
         enterMessagetextfield.resignFirstResponder()
         mailButton.setTitle("Mail sent", forState: UIControlState.Normal)
         
+    }
+    @IBAction func nameTextFieldEntered(sender: UITextField) {
+        mailButton.setTitle("Send Mail", forState: UIControlState.Normal)
+    }
+    
+    
+    @IBAction func messageTextFieldEntered(sender: UITextField) {
+        mailButton.setTitle("Send Mail", forState: UIControlState.Normal)
     }
 
 }
